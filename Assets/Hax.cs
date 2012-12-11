ad3c78d72c54ff1a7949fbf1452f162e2442b36a
Jason Mitchell <foobardog@hotmail.com>
2012-12-10 21:31:33 -0800

    ScriptFile check

---
+++ Assets/Hax.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Hax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// There is nothing here but a comment.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
